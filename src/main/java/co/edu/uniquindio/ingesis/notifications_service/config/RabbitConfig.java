6f62e1133490d458079b29b307ac49004a77c0d6
Juan Camilo Astudillo <112102340+CamiloAst@users.noreply.github.com>
2025-09-17 10:18:21 -0500

    Update RabbitConfig.java

--- src/main/java/co/edu/uniquindio/ingesis/notifications_service/config/RabbitConfig.java
+++ src/main/java/co/edu/uniquindio/ingesis/notifications_service/config/RabbitConfig.java
@@ -6,7 +6,7 @@ import org.springframework.amqp.core.DirectExchange;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Queue;
+import org.springframework.amqp.core.Queue;
 
 @Configuration
 public class RabbitConfig {
